04759ebbe4a7e0ee791673575fb62be0046da0fe
lamductan <lamductan@gmail.com>
2018-07-27 23:25:25 +0700

    Fix bug

---
+++ downloadAndExtractData.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+filename=$1 # Example: MOD13Q1
+
+if [[ ! -d $filename ]]; then
+    mkdir $filename
+fi
+cd $filename
+ 
+{
+    read
+    while IFS=, read -r id site_id product latitude longitude email start_date end_date kmAboveBelow kmLeftRight order_uid
+    do 
+        if [[ ! -d $id ]]; then
+            if [[ ! -f $id.tar.gz ]]; then
+                wget $order_uid/tif/GTiff.tar.gz -O $id.tar.gz
+            fi
+            ./../extract.sh $id
+            rm -rf $id.tar.gz
+        fi
+    done
+} < ../$filename.csv
---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+./downloadAndExtractData.sh MOD13Q1
+./downloadAndExtractData.sh MYD13Q1
